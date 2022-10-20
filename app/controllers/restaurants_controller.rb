e5f9594ab49294175ae0f1abc2e0d21be4ff2431
angelee otic <otic.angeleemae@gmail.com>
2022-10-20 20:27:10 +0100

    reviews and its content

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,4 +10,20 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
 end
